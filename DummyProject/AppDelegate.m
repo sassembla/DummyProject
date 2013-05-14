7f817cc1e40350cef839c0641a5797f68c814e97
sassembla <toru.inoue@kissaki.tv>
2013-05-14 17:04:18 +0900

    add code

--- DummyProject/AppDelegate.m
+++ DummyProject/AppDelegate.m
@@ -8,11 +8,36 @@
 
 #import "AppDelegate.h"
 
-@implementation AppDelegate
+@implementation AppDelegate {
+    SEL m_receiver;
+}
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
+    m_receiver = @selector(receiver:);
+    
     // Insert code here to initialize your application
+    IMP func = [self methodForSelector:m_receiver];
+    (* func)(self, m_receiver, aNotification);
+}
+
+- (void) receiver:(NSNotification * )notif {
+    NSLog(@"hereComes %@", notif);
+    
+    int a = 0;
+    
+    switch (a) {
+        case 0:{
+            break;
+        }
+        case -1:{
+            NSDictionary * dict = @{@"key":@"val"};
+            break;
+        }
+            
+        default:
+            break;
+    }
 }
 
 @end
